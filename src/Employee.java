f8e65a081f7f4459e8e89e7aa3a20b2857b42869
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-19 11:00:42 +0530

    ADDED : Code to check employee attendance

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,11 @@
 public class Employee {
-
+    public static final int FULL_TIME_PRESENT = 1;
+    public static final int ABSENT = 0;
+    public int isPresent() {
+        int attendance = (int) (Math.floor(Math.random() * 10) % 2);
+        if (attendance == FULL_TIME_PRESENT)
+            return FULL_TIME_PRESENT;
+        else
+            return ABSENT;
+    }
 }
