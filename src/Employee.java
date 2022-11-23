ba1d11a9321a9d8260ab1e4bc1a93776fd2c1535
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-23 22:24:01 +0530

    ADDED : Code to calculate daily wage

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,27 @@
+import javax.print.StreamPrintServiceFactory;
+
 public class Employee {
     public static final int FULL_TIME_PRESENT = 1;
+    public static final int PART_TIME_PRESENT = 2;
     public static final int ABSENT = 0;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
+
     public int isPresent() {
-        int attendance = (int) (Math.floor(Math.random() * 10) % 2);
+        int attendance = (int) Math.floor(Math.random() * 3);
         if (attendance == FULL_TIME_PRESENT)
             return FULL_TIME_PRESENT;
+        if (attendance == PART_TIME_PRESENT)
+            return PART_TIME_PRESENT;
         else
             return ABSENT;
+    }
+
+    public int getDailyWage(int attendance) {
+        if (attendance == FULL_TIME_PRESENT)
+            return WAGE_PER_HOUR * FULL_DAY_HOURS;
+        else
+            return WAGE_PER_HOUR * PART_TIME_HOURS;
     }
 }
--- src/EmployeeWageComputer.java
+++ src/EmployeeWageComputer.java
@@ -1,11 +1,16 @@
 public class EmployeeWageComputer {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computer");
+
         Employee empObj = new Employee();
         int attendance = empObj.isPresent();
-        if(attendance==Employee.FULL_TIME_PRESENT)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+
+        if (attendance > Employee.ABSENT) {
+            int dailyWage = empObj.getDailyWage(attendance);
+            System.out.println("Daily Wage = " + dailyWage);
+        } else {
+            System.out.println("Employee is Absent");
+            System.out.println("Daily Wage = 0");
+        }
     }
 }
