7722a9e908eb1b4dc48b3c93f6bbc7ac743ad746
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-24 07:31:32 +0530

    ADDED : Code to calculate monthly wage and wage for 20 days or 100 working hours

--- src/Employee.java
+++ src/Employee.java
@@ -8,15 +8,16 @@ public class Employee {
     public static final int FULL_DAY_HOURS = 8;
     public static final int PART_TIME_HOURS = 4;
     public static final int MONTHLY_WORKING_DAYS = 20;
-
-    private int monthlyWage;
+    private static final int MAX_WORKING_HOURS = 100;
 
     public int getMonthlyWage() {
-        return monthlyWage;
-    }
-
-    public void setMonthlyWage(int monthlyWage) {
-        this.monthlyWage = monthlyWage;
+        int totalMonthlyWage = 0;
+        for (int i = 0; i < MONTHLY_WORKING_DAYS; i++) {
+            int attendance = isPresent();
+            int dailyWage = getDailyWage(attendance);
+            totalMonthlyWage = totalMonthlyWage + dailyWage;
+        }
+        return totalMonthlyWage;
     }
 
     public int isPresent() {
@@ -32,10 +33,25 @@ public class Employee {
     public int getDailyWage(int attendance) {
         if (attendance == FULL_TIME_PRESENT)
             return WAGE_PER_HOUR * FULL_DAY_HOURS;
-        else if (attendance==PART_TIME_PRESENT)
+        else if (attendance == PART_TIME_PRESENT)
             return WAGE_PER_HOUR * PART_TIME_HOURS;
         else
             return 0;
     }
 
+    public int getWageForMaxDays() {
+        int totalWorkingHours = 0;
+        int totalWorkingDays = 0;
+        int totalWage = 0;
+        while (totalWorkingHours <= MAX_WORKING_HOURS && totalWorkingDays < MONTHLY_WORKING_DAYS) {
+            int attendance = isPresent();
+            if (attendance == FULL_TIME_PRESENT)
+                totalWorkingHours += FULL_DAY_HOURS;
+            if (attendance == PART_TIME_PRESENT)
+                totalWorkingHours += PART_TIME_HOURS;
+            totalWage = totalWage + getDailyWage(attendance);
+            totalWorkingDays++;
+        }
+        return totalWage;
+    }
 }
--- src/EmployeeWageComputer.java
+++ src/EmployeeWageComputer.java
@@ -1,14 +1,12 @@
 public class EmployeeWageComputer {
     public static void main(String[] args) {
-        int totalWage = 0;
         System.out.println("Welcome to Employee Wage Computer");
-
+        int monthlyWage = 0;
+        int totalWage = 0;
         Employee empObj = new Employee();
-        for (int i = 0; i < Employee.MONTHLY_WORKING_DAYS; i++) {
-            int attendance = empObj.isPresent();
-            int dailyWage = empObj.getDailyWage(attendance);
-            totalWage = totalWage + dailyWage;
-        }
-        System.out.println("Total wage = " + totalWage);
+        monthlyWage = empObj.getMonthlyWage();
+        System.out.println("Total monthly wage = " + monthlyWage);
+        totalWage = empObj.getWageForMaxDays();
+        System.out.println("Total wage till max condition fullfills : " + totalWage);
     }
 }
